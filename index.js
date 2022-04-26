acbfe9b724dfd044fe17c167747a6ecb469e09a5
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-04-26 09:54:31 -0400

    Basic done

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+let usernames = new Set();
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'minhle123',
+    database: 'Users',
+});
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
 })
-app.get("/", (request, response) => {
-    response.send("Hello World")
+app.post("/sign-in", (request, response) => {
+    let username = request.body.username;
+    let password = request.body.password;
+    db.query("SELECT * FROM Users.users WHERE username=(?);", username, (error, result) => {
+        if (result) {
+            if (result[0].password === password) {
+                response.status(200).send(`Welcome ${username}`);
+            }
+            else {
+                response.status(400).send("Incorrect password")
+            }
+        }
+        else {
+            console.log("ABC")
+        }
+    })
+})
+
+app.post("/sign-up", (request, response) => {
+    let username = request.body.username;
+    let password = request.body.password;
+    if (usernames.has(username)) {
+        response.status(400).send("User already existed");
+    }
+    else {
+        db.query("INSERT INTO users (username, password) VALUES (?, ?);", [username, password], (error, result) => {
+            if (error) {
+                response.status(400).send("Fail to sign up");
+            }
+            else {
+                response.status(201).send("Welcome new user");
+                usernames.add(username);
+            }
+        })
+    }
 })
