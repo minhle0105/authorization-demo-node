8055d75ff4e0181c20ab90ffa138632d79de70ac
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-04-27 00:04:25 -0400

    Encrypt password

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
+const e = require("express");
 const saltRounds = 10;
 
 app.use(cors());
@@ -24,21 +25,24 @@ const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
 })
+
 app.post("/sign-in", (request, response) => {
     let username = request.body.username;
     let password = request.body.password;
     db.query("SELECT * FROM Users.users WHERE username=(?);", username, (error, result) => {
-        if (result) {
-            if (result[0].password === password) {
-                response.status(200).send({
-                    message: `Welcome ${username}`
-                });
-            }
-            else {
-                response.status(400).send({
-                    message: "Incorrect password"
-                })
-            }
+        if (result.length > 0) {
+            bcrypt.compare(password, result[0].password, (e, r) => {
+                if (r) {
+                    response.status(200).send({
+                        message: `Welcome ${username}`
+                    });
+                }
+                else {
+                    response.status(400).send({
+                        message: "Incorrect password"
+                    })
+                }
+            })
         }
         else {
             response.status(400).send({
@@ -52,23 +56,32 @@ app.post("/sign-up", (request, response) => {
     let username = request.body.username;
     let password = request.body.password;
 
-    db.query("SELECT * from Users.users WHERE username=(?);", username, (error, result) => {
-        if (result) {
-            response.status(400).send({
-                message: "User Already Registered"
-            })
+    bcrypt.hash(password, saltRounds, (error, hash) => {
+        if (error) {
+            console.log(error)
         }
         else {
-            db.query("INSERT INTO users (username, password) VALUES (?, ?);", [username, password], (error, result) => {
-                if (error) {
+            db.query("SELECT * from Users.users WHERE username=(?);", username, (error, result) => {
+                if (result.length > 0) {
+                    console.log(result.length)
                     response.status(400).send({
-                        message: "Fail to Sign In"
-                    });
+                        message: "User Already Registered"
+                    })
                 }
                 else {
-                    response.status(201).send({
-                        message: "Welcome new user"
-                    });
+                    db.query("INSERT INTO users (username, password) VALUES (?, ?);", [username, hash], (error, result) => {
+                        if (error) {
+                            console.log(error)
+                            response.status(400).send({
+                                message: "Fail to Sign Up"
+                            });
+                        }
+                        else {
+                            response.status(201).send({
+                                message: "Welcome new user"
+                            });
+                        }
+                    })
                 }
             })
         }
